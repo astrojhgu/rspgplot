df1b787a209cdbe43b59bf63dd298bd2f601256f
GU Junhua <jhgu@nao.cas.cn>
2020-10-22 20:09:26 +0800

    modified:   src/lib.rs 	modified:   src/main.rs 	modified:   src/types.rs

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,10 @@
 extern crate rspgplot;
-
+use rspgplot::*;
 fn main() {
-    let xs=vec![1.,2.,3.,4.,5.];
-    let ys:Vec<_>=xs.iter().map(|&x| x*x).collect();
-    rspgplot::pgopen(&"?");
-    let (w,h)=rspgplot::pgqvsz(1);
-    println!("{} {}", w, h);
-    rspgplot::pgpap(15., 2.0/3.0);
-    rspgplot::pgslw(10);
-    rspgplot::pgenv(0.0, 6., -4.0, 30., 0, 0);
-    
-    rspgplot::pglab("(x)", "(y)", "PGPLOT Example 1:  y = x\\u2");
-    rspgplot::pgpt(&xs, &ys, 2);
-    rspgplot::pgsls(3);
-    rspgplot::pgline(&xs, &ys);
-    let mut x=0.0;
-    let mut y=0.0;
-    rspgplot::pgcurs(&mut x, &mut y);
-    rspgplot::pgend();
+    init_dev("?").unwrap();
+    set_paper_size(16.0, 13.0);
+    init_env((-1,4), (-1,4), types::Just::NonEqual, types::AxesStyle::WithLabels);
+    scatter(&vec![1,2,3], &vec![1,2,3], 3);
+    plot(&vec![1,2,3], &vec![1,2,3]);
+    pgclos();
 }
--- src/types.rs
+++ src/types.rs
@@ -17,3 +17,46 @@ pub struct HatchingStyle{
     pub sepn:f32,
     pub phase: f32,
 }
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum Just{
+    Equal,
+    NonEqual,
+}
+
+impl Just{
+    pub fn to_i32(self)->i32{
+        match self{
+            Just::Equal=>1,
+            _=>0,
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum AxesStyle{
+    Blank,
+    BoxOnly,
+    WithLabels,
+    WithLabelsAndAxis0,
+    WithLabelsAxis0AndGrid,
+    LogX,
+    LogY,
+    LogXY
+}
+
+impl AxesStyle{
+    pub fn to_i32(self)->i32{
+        use AxesStyle::*;
+        match self{
+            Blank=>-2,
+            BoxOnly=>-1,
+            WithLabels=>0,
+            WithLabelsAndAxis0=>1,
+            WithLabelsAxis0AndGrid=>2,
+            LogX=>10,
+            LogY=>20,
+            LogXY=>30,
+        }
+    }
+}
